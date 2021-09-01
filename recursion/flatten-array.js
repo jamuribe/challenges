e9f0e90512c6cbe7535b41128ad240ff7db81e98
Jose A. Molina <josemouribe@gmail.com>
2021-09-01 09:15:58 +0200

    Steamroller

---
+++ recursion/flatten-array.js
@@ -0,0 +1,19 @@
+// Steamroller
+// Flatten a nested array.You must account for varying levels of nesting.
+
+function steamrollArray(arr) {
+  let result = [];
+  function recur(array) {
+    for (let i = 0; i < array.length; i++) {
+      if (Array.isArray(array[i])) {
+        recur(array[i])
+      } else {
+        result.push(array[i]);
+      }
+    }
+  }
+  recur(arr)
+  return result;
+}
+
+steamrollArray([1, [2], [3, [[4]]]]);
