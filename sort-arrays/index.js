fe119ffdb4efd6b06954578932e62cd1471dd203
Jose A. Molina <josemouribe@gmail.com>
2021-09-16 10:46:10 +0200

    Insertion sort

--- sort-arrays/index.js
+++ sort-arrays/index.js
@@ -16,3 +16,20 @@ function bubbleSort(arr) {
 
 }
 console.log(bubbleSort([3, 5, 2, 5, 6]))
+
+function insertionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+
+    for (let o = i; o > 0; o--) {
+      if (arr[o] > arr[o - 1]) continue;
+      if (arr[o] < arr[o - 1]) {
+        [arr[o - 1], arr[o]] = [arr[o], arr[o - 1]]
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(insertionSort([6, 5, 4, 3, 2, 1, 0]))
+
+module.exports = insertionSort;
