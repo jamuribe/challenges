d700626c8a47c907f5792fab2da9677b0fa88cff
Jose A. Molina <josemouribe@gmail.com>
2021-09-18 09:42:27 +0200

    Sum of strings

--- sum-of-primes/index.js
+++ sum-of-primes/index.js
@@ -3,7 +3,7 @@
 
 // Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
 
-function sumPrimes (num) {
+function sumPrimes(num) {
   let result = 0;
   for (let i = 2; i <= num; i++) {
     if (checkPrime(i)) {
@@ -13,7 +13,7 @@ function sumPrimes (num) {
   return result;
 }
 
-function checkPrime (num) {
+function checkPrime(num) {
   for (let i = 2; i < num; i++) {
     if (num % i === 0) {
       return false;
@@ -23,3 +23,48 @@ function checkPrime (num) {
 }
 
 sumPrimes(10);
+
+// Sum of two strings. In the test there are really big numbers remember that
+
+function sumStrings(a, b) {
+  var A = a.split(""), B = b.split(""), C = 0, R = "";
+
+  while (A.length || B.length || C) {
+    C = C + (~~A.pop() + ~~B.pop());
+    R = (C % 10) + R;
+    C = C > 9;
+  }
+
+  return R.replace(/^0+/, "");
+}
+
+// ~~it's using for convert any value to integer. For example:
+
+// ~~undefined = 0, ~~1.1 = 1, ~~"test" = 0 and even ~~null = 0 etc.
+
+//   It's good hack if you don't want write various conditions such as
+
+//     if (a != null || a != undefined) you just write c = ~~a and if variable is incorrect,
+//       it's just return 0 in any case without any exception.
+
+// C = C > 9 probably it's something like carry flag. There are column addition like as
+
+// 4 +
+//   4
+// 8`
+
+// you get just 8 and addition is over, but if you add
+
+// 5 +
+// 5
+// 10
+
+// You add 5 + 5 and get 10, but you can write only one digit, the 0. You have to write 1 too.
+// But cycle is over because A and B arrays already empty. And result will be 0.
+// In this case you need check if you have reminder from adding.
+
+// C = C > 9 just check if number more 9. I.e C = 5 + 5, C = 10, and then C = 10 > 9, C = true.
+
+// In this case a script have to run more one cycle.
+// And C now is Boolean, true or false then C = C + (~~A.pop() + ~~B.pop()); considering as C = 1 + (number + number) or C = 0 + (number + number)
+// I.e true + 1 == 2 or true + 0 == 1
