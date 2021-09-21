cc6139d9b8d1a3804ca67ea1907fb8da75e08d99
Jose A. Molina <josemouribe@gmail.com>
2021-09-21 12:03:47 +0200

    Deleting unused code

--- sort-arrays/index.js
+++ sort-arrays/index.js
@@ -29,24 +29,6 @@ function insertionSort(arr) {
 
 console.log(insertionSort([6, 5, 4, 3, 2, 1, 0]));
 
-function mergeSort(arr) {
-  console.log(arr, 'this is array');
-  let l, r;
-  l = arr.slice(0, Math.round(arr.length / 2));
-  r = arr.slice(Math.round(arr.length / 2));
-  if (l < r) {
-    mergeSort(l);
-    // mergeSort(r)
-  }
-  // merging(l, r)
-  // console.log(l, 'its ame R', r)
-}
-
-function merging(l, r) {
-
-}
-
-mergeSort([0, 2, 41, 2, 25, 52, 17, 5, 81, 6, 3, 4, 52, 22, 23]);
 
 // MERGE SORT
 
